cfcb8f47e7eb62fb7d847b9f5136cc906e8a6b59
Qianlong <52986441+qianlong0502@users.noreply.github.com>
2025-04-10 09:19:23 +0800

    Update compose.py: fix bug

--- latent-diffusion/ldm/modules/losses/compose.py
+++ latent-diffusion/ldm/modules/losses/compose.py
@@ -1,5 +1,4 @@
 from re import S
-from ldm.modules.losses.frequency_loss import SpecAwareLoss
 from torch import nn
 
 class ComposeLoss(nn.Module):
@@ -13,9 +12,5 @@ class ComposeLoss(nn.Module):
     def forward(self, pred, target, ref=None):
         loss_value = 0.0
         for i in range(self.number_loss):
-            if isinstance(self.losses[i], SpecAwareLoss):
-                assert ref is not None
-                loss_value += self.losses[i](pred, target, ref).mean() * self.weights[i]
-            else:
-                loss_value += self.losses[i](pred, target).mean() * self.weights[i]
+            loss_value += self.losses[i](pred, target).mean() * self.weights[i]
         return loss_value
